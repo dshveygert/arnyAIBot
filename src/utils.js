3e706bd9a91a4a9fefc5bdf7bfebb0e1a9c91eee
dshveigert <5706601+dshveygert@users.noreply.github.com>
2023-05-29 22:54:08 +0700

    Filter users by Telegram nickname

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,12 @@
 import config from 'config';
 
 export const accessDenied = ({message}) => {
-  const whiteList = config.get('USER_WHITE_LIST');
+  let whiteList;
+  try {
+    whiteList = config.get('USER_WHITE_LIST');
+  } catch (err) {
+    return true;
+  }
   if (!!whiteList && whiteList.length > 0) {
     return !(!!message && !!message.from.username && !!whiteList.includes(message.from.username));
   }
